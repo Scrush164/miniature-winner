64b53c0a0cbb43230f06b45f3129a954d96615ad
Scrush164 <stuffz597@gmail.com>
2026-06-28 23:24:20 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const { PrismaClient } = require('@prisma/client');
+
+
+
 
 const app = express();
 const dataFile = path.join(process.cwd(), 'data.json');
@@ -8,14 +10,7 @@ const dataFile = path.join(process.cwd(), 'data.json');
 app.use(express.json());
 app.use(express.static('public'));
 
-// Initialize data.json if it doesn't exist
-const initializeData = () => {
-  if (!fs.existsSync(dataFile)) {
-    fs.writeFileSync(dataFile, JSON.stringify({ assets: [], liabilities: [] }, null, 2));
-  }
-};
 
-initializeData();
 
 const publicPath = path.join(__dirname, 'public');
 console.log('Serving static files from:', publicPath);
@@ -29,12 +24,16 @@ app.get('/', (req, res) => {
 });
 
 //GET and POST Routes
-app.get('/api/data', (req, res) => {
+app.get('/api/data', async (req, res) => {
   try {
-    const data = JSON.parse(fs.readFileSync(dataFile, 'utf8'));
-    res.json(data);
+    // Fetch assets and liabilities from the database
+    const transaction = await prisma.transaction.findMany();
+    
+    
+    res.json({transaction});
   } catch (err) {
-    res.json({ assets: [], liabilities: [] });
+    console.error(err);
+    res.status(500).json({ error: 'Failed to fetch data' });
   }
 });
 
@@ -42,13 +41,22 @@ app.post('/api/data', (req, res) => {
   fs.writeFileSync(dataFile, JSON.stringify(req.body, null, 2));
   res.json({ success: true });
 });
-//If it can't find the route. Maybe its not going to the public
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  const {name, amount, type} = req.body;
+     const newTransaction = await prisma.transaction.create({
+      data: {
+        name,
+        amount: parseFloat(amount),
+        type // "asset" or "liability"
+      }
 });
 
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+process.on('beforeExit', async () => {
+  await prisma.$disconnect();
 });
