7637c15625ce208c0314f6ad1fe5114b39e49f12
Scrush164 <stuffz597@gmail.com>
2026-07-12 21:48:47 -0400

    Add try-catch block for transaction creation

--- src/app/api/submit/route.ts
+++ src/app/api/submit/route.ts
@@ -6,7 +6,7 @@ export async function POST(request: Request) {
   const body = await request.json()
   const { assets, liabilities, netWorth } = body
 
-
+try{
     await prisma.Transaction.create({
       data: {
         amount: body.amount,
