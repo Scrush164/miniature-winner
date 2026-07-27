fbf9e4485c19808b394c960ee0c0f23ce98ab7f8
Scrush164 <stuffz597@gmail.com>
2026-07-26 23:37:58 -0400

    Remove Login and SignUp links from navbar
    Removed Login and SignUp links from the navbar.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,8 +64,6 @@ export default function Home() {
       <header className="navbar">
         <h1 className="logo">Summer Project.</h1>
         <nav>
-          <a href="#">Login</a>
-         <a href="#" className="btn">SignUp</a>
         </nav>
       </header>
 
